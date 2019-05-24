17cdbbef3264c57567c85d31223481f7b7108b7b
Mussab Tanveer <mussab.tanveer9@gmail.com>
2019-05-25 00:18:38 +0500

    shared ingredient model class

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,13 @@
+export class Ingredient {
+  // long method
+  /* public name: string;
+  public amount: number;
+
+  constructor(name: string, amount: number) {
+    this.name = name;
+    this.amount = amount;
+  } */
+
+  // short method
+  constructor(public name: string, public amount: number) {}
+}
